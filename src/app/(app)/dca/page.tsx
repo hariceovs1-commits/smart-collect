0efaa2451a54d5a16d03127b0e2523dde52a6092
Hari Prasanth L <hari.ceo.vs1@gmail.com>
2026-01-07 17:59:26 +0000

    add option to add a mode of communicaion (_for element <CardContent>_)

--- src/app/(app)/dca/page.tsx
+++ src/app/(app)/dca/page.tsx
@@ -64,7 +64,7 @@ export default function DcaDashboard() {
       return null;
   }
   
-  const myCases = cases.filter(c => c.assignedDcaId === loggedInUser.id);
+  const myCases = cases.filter(c => c.assignedDcaId === loggedInUser.id && !c.feedback);
   const myTimetable = timetable.filter(t => t.dcaId === loggedInUser.id);
   
   const handleSuggestMode = async () => {
